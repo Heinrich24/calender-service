78f080eec09bfdf5d7c0fcbcabbf3756648555c8
Heinrich Dedekind <heinrich.dedekind@gmail.com>
2020-09-28 12:54:02 +0200

    Add comments and logic to print the name of week day to console.

--- src/com/scooby24/main/MyMain.java
+++ src/com/scooby24/main/MyMain.java
@@ -3,12 +3,21 @@ package com.scooby24.main;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.GregorianCalendar;
+import java.time.DayOfWeek;
+import java.time.LocalDate;
 
+/*
+ * The following class invokes most of the calender related fields and prints them to the console
+ */
 public class MyMain {
 
 	public static void main(String[] args) {
 		Calendar calendar = new GregorianCalendar();
 	    calendar.setTime(new Date());
+
+	    
+        LocalDate localDate = LocalDate.now(); 
+	    DayOfWeek dayOfWeek = DayOfWeek.from(localDate);
 
 	    System.out.println("ERA: " + calendar.get(Calendar.ERA));
 	    System.out.println("YEAR: " + calendar.get(Calendar.YEAR));
@@ -23,7 +32,7 @@ public class MyMain {
 	    System.out.println("MILLISECOND: " + calendar.get(Calendar.MILLISECOND));
 	    System.out.println("ZONE_OFFSET: "+ (calendar.get(Calendar.ZONE_OFFSET)/(60*60*1000)));
 	    System.out.println("DST_OFFSET: "+ (calendar.get(Calendar.DST_OFFSET)/(60*60*1000)));
-//
+	    System.out.println("DAY_OF_THE_WEEK_NAME: "+ (dayOfWeek.name()));
 	}
 
 }
